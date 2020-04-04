82cc937f33f5819d40cca6ecce960473dec42a28
Tom Molotnikoff <tdmolotnikoff1@sheffield.ac.uk>
2020-04-05 00:20:32 +0100

    saved the js from callback hell and added skeleton for 3 more pages

--- js/script.js
+++ js/script.js
@@ -1,145 +1,189 @@
+
 $(document).ready(function(){
-    var lineNumbers = document.getElementsByClassName("lineNumber");
-    for (i = 0; i < lineNumbers.length; i++) {
-        if(i<10) {lineNumbers[i].innerHTML = "0" + i}
-        else {lineNumbers[i].innerHTML = i;}
+
+    // Cache all the selectors - save time and bugs
+    var lineNumbers = $(".lineNumber");
+    var aboutExpander = $(".aboutExpander");
+    var insideAbout = $("#insideAbout");
+    var aboutHidden = $("#aboutHidden");
+    var linksExpander = $(".linksExpander");
+    var insideLinks = $("#insideLinks");
+    var linksHidden = $("#linksHidden");
+    var type = $(".type");
+    var typeSlow = $(".typeSlow");
+    var typeSlowest = $(".typeSlowest");
+    var expander = $(".expander");
+    var aboutMe = $("#aboutMe");
+    var education = $("#education");
+    var projects = $("#projects");
+    var pastWork = $("#pastWork");
+    var pages = $(".page");
+    var scrollMore = $(".scrollMore");
+    var scrollEducation = $("#scrollEducation");
+    var scrollProjects = $("#scrollProjects");
+    var scrollPastWorkPage = $("#scrollPastWorkPage ");
+    var contactMe = $("#contactMe");
+
+    // Handler for aboutMe function slide down and slide up
+    aboutExpander.click(function() {expanderTransition(insideAbout, aboutHidden);});
+    // Handler for pageLinks function slide down and slide up
+    linksExpander.click(function() {expanderTransition(insideLinks, linksHidden);});
+
+    // Expand a code block out or in
+    function expanderTransition(inside, hidden){
+        if (!inside.is(":visible")) {
+            hidden.fadeOut("fast", function() {inside.slideDown("fast");});
+        } else {
+            inside.slideUp("fast", function() {hidden.fadeIn("fast")});
+        }
     }
-});
 
-var aboutExpander;
-var insideAbout;
-var aboutHidden;
-var linksExpander;
-var insideLinks;
-var linksHidden;
+    // Perform the typing transition
+    // False = type backwards
+    // True = type forwards
+    // Callback function is executed after the longest animation
+    // cbPageTo* are there explicitly for the swapPagesTransition as it is used frequently    
+    function typeTransition(direction, callback, cbPageToClose, cbPageToOpen) {
+        callback = callback || false;
+        cbPageToClose = cbPageToClose || false;
+        cbPageToOpen = cbPageToOpen || false;
+        type.stop();typeSlow.stop();typeSlowest.stop();expander.stop();
+        var endWidth;
+        if (direction) {endWidth = "100%";}
+        else {endWidth = "0%";}
 
-$(document).ready(function(){
+        expander.animate({opacity: endWidth}, 200);
+        type.animate({width: endWidth}, 750);
+        typeSlow.animate({width: endWidth}, 1000);
 
-    aboutExpander = $(".aboutExpander");
-    insideAbout = $("#insideAbout");
-    aboutHidden = $("#aboutHidden");
-
-    aboutExpander.click(function() {
-        if (insideAbout.is(":visible")) {
-            insideAbout.slideUp("fast", function() {
-                aboutHidden.fadeIn("fast");
-            });
-        } else {
-            aboutHidden.fadeOut("fast", function() {
-                insideAbout.slideDown("fast");
-            });
-        }
-    });
-});
-
-$(document).ready(function(){
-
-    linksExpander = $(".linksExpander");
-    insideLinks = $("#insideLinks");
-    linksHidden = $("#linksHidden");
-
-    linksExpander.click(function() {
-        if (insideLinks.is(":visible")) {
-            insideLinks.slideUp("fast", function() {
-                linksHidden.fadeIn("fast");
-            });
-        } else {
-            linksHidden.fadeOut("fast", function() {
-                insideLinks.slideDown("fast");
+        if (!callback) {typeSlowest.animate({width: endWidth}, 1500);} 
+        else {
+            typeSlowest.animate({width: endWidth}, 1500, function() {
+                if (cbPageToOpen && cbPageToClose) {callback(cbPageToClose, cbPageToOpen);}
+                else {callback();}
             });
         }
-    });
-}); 
+    }
 
-var type;
-var typeSlow;
-var typeSlowest;
-var typeSpecial;
-var expander;
-var education;
-var aboutMe;
+    // Fades one page out, then the other in
+    function swapPagesTransition(pageToClose, pageToOpen) {
+        pages.not(pageToClose).hide();
+        pageToClose.fadeOut(200, function() {
+            pageToOpen.fadeIn(200);
+            updateBottomPrompt();
+        });
+    }
 
-$(document).ready(function() {
-    type = $(".type");
-    typeSlow = $(".typeSlow");
-    typeSlowest = $(".typeSlowest");
-    typeSpecial = $(".typeSpecial");
-    expander = $(".expander");
-    education = $("#education");
-    aboutMe = $("#aboutMe");
+    function updateBottomPrompt() {
+        if (aboutMe.is(":visible")){
+            scrollMore.not(scrollProjects).hide();
+            scrollProjects.fadeOut("fast");
+            scrollEducation.fadeIn("fast");
+        } else if (education.is(":visible")) {
+            scrollEducation.fadeOut("fast");
+            scrollProjects.fadeIn("fast");
+            scrollPastWorkPage.fadeOut("fast");
+            contactMe.hide();
+        } else if (projects.is(":visible")) {
+            scrollEducation.hide();
+            scrollProjects.fadeOut("fast");
+            scrollPastWorkPage.fadeIn("fast");
+            contactMe.fadeOut("fast");
+        } else if (pastWork.is(":visible")) {
+            scrollMore.not(scrollPastWorkPage).hide();
+            scrollPastWorkPage.fadeOut("fast");
+            contactMe.fadeIn("fast");
+        }
+    }
 
-    type.animate({width: '100%'}, 750);
-    typeSlow.animate({width: '100%'}, 1000);
-    typeSlowest.animate({width: '100%'}, 1500);
-    typeSpecial.animate({width: '100%'}, 500);
-    $(".scrollEducation").fadeIn("fast");
-});
+    // This scroll block needs to display the aboutMe page
+    function firstScrollBlock() {
+        if (newVal < oldVal && newVal != oldVal) {
+            if (education.is(":visible")) {swapPagesTransition(education, aboutMe);}
+            typeTransition(true);
+        }
+    }
 
+    // This scroll block needs to display the educationPage
+    function secondScrollBlock() {
+        // Are we moving into this from previous or coming back from the next?
+        if (newVal > oldVal && newVal != oldVal) {
+            typeTransition(false, swapPagesTransition, aboutMe, education);//
+        } else if (newVal != oldVal){
+            swapPagesTransition(projects, education);
+        }
+    }
 
-var newVal = 0
-var oldVal = 0;
-document.addEventListener("wheel", function (e) {
-    // to make it work on IE or Chrome
-    
-        var variation = parseInt(e.deltaY);
-        
-        newVal = oldVal + variation;
-        if (newVal < 0) {newVal=0;}
-        if (newVal > 101) {newVal=101;}
+    // This scroll block needs to display the projectsPage
+    function thirdScrollBlock() {
+        if (newVal > oldVal && newVal != oldVal) {
+            swapPagesTransition(education, projects);
+        } else if (newVal != oldVal) {
+            swapPagesTransition(pastWork, projects);
+        }
+    }    
 
+    // This scroll block needs to display the pastWorkPage
+    function fourthScrollBlock() {
+        if (newVal != oldVal) {swapPagesTransition(projects, pastWork);}
+    }
+
+    function capScrolls() {
+        if (aboutMe.is(":visible")){
+            currentCap = 150;
+            currentMin = 0;
+        } else if (education.is(":visible")) {
+            currentCap = 250;
+            currentMin = 50;
+        } else if (projects.is(":visible")) {
+            currentCap = 350;
+            currentMin = 150;
+        } else if (pastWork.is(":visible")) {
+            currentCap = 450;
+            currentMin = 250;
+        }
+    }
+
+    var newVal = 0
+    var oldVal = 0;
+    var currentCap = 150;
+    var currentMin = 0;
+    function scrollBehaviour(e) {
+        // Find the new scroll value
+        var movement = parseInt(e.deltaY);
+        newVal = oldVal + movement;
+        capScrolls();
+        // Floor and roof for the scroll wheel values
+        if (newVal < currentMin) {newVal=currentMin;}
+        if (newVal > currentCap) {newVal=currentCap;}
+        // Each section is a page, the scrollblock functions switch the active page
         if (newVal < 100) {
-            if (education.is(":visible")) {
-                education.fadeOut(200, function(){
-                    aboutMe.fadeIn(100);
-                });
-            }
-
-            if (newVal < oldVal) {
-                type.stop();
-                typeSlow.stop();
-                typeSlowest.stop();
-                expander.stop();
-                expander.animate({opacity: '100%'}, 200);
-                type.animate({width: '100%'}, 1000);
-                typeSlow.animate({width: '100%'}, 1500);
-                typeSlowest.animate({width: '100%'}, 2000);
-            }
-
-            if($(".scrollProjects").is(":visible")){
-                $(".scrollProjects").fadeOut("fast", function(){
-                    $(".scrollEducation").fadeIn("fast");
-                });
-            }
-        } else {
-
-            if (newVal > oldVal) {
-                type.stop();
-                typeSlow.stop();
-                typeSlowest.stop();
-                expander.stop();
-                expander.animate({opacity: '0%'}, 200);
-                type.animate({width: '0%'}, 750, function(){
-                    
-                });
-                typeSlow.animate({width: '0%'}, 1000);
-                typeSlowest.animate({width: '0%'}, 1500, function(){
-                    //aboutMe.animate({height: '0px'}, 200, function(){
-                        aboutMe.fadeOut(200, function() {
-                            education.fadeIn(200);
-                        });
-                        
-                    //});
-                });
-            }
-
-
-            if($(".scrollEducation").is(":visible")){
-                $(".scrollEducation").fadeOut("fast", function(){
-                    $(".scrollProjects").fadeIn("fast");
-                });
-            }
-        } 
+            firstScrollBlock();
+        } else if (newVal < 200) {
+            secondScrollBlock();
+        } else if (newVal < 300) {
+            thirdScrollBlock();
+        } else if (newVal < 400) {
+            fourthScrollBlock();
+        }
+        // Keep track of the last scroll to determine direction
         oldVal = newVal;
-        return false;
+    }
 
-}, true);
+
+    // Draw the first page
+    function init(){
+        aboutMe.show();
+        // Write the line numbers into the HTML for the first page - eases editing
+        for (i = 0; i < lineNumbers.length; i++) {
+            if(i<10) {$(lineNumbers[i]).html("0" + i);}
+            else {$(lineNumbers[i]).html(i);}
+        }
+        // Fade in the page and then callback to enable the scrolling
+        typeTransition(true, function() {document.addEventListener("wheel", scrollBehaviour, true);});
+        linksHidden.fadeIn("fast");
+        scrollEducation.fadeIn("fast");
+    }
+    // Initialise
+    init();
+});
