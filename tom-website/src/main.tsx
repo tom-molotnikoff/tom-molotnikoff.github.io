1c745a2fe2dd01d1a54800a6188c7371930da769
Tom Molotnikoff <tom.molotnikoff@cirata.com>
2025-09-05 23:14:47 +0100

    Added support for Github flavoured markdown

--- tom-website/src/main.tsx
+++ tom-website/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./blog-mdx.css";
 import App from "./App";
 
 // This is the entry point of the React application.
--- tom-website/vite.config.mjs
+++ tom-website/vite.config.mjs
@@ -1,26 +1,28 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import remarkFrontmatter from 'remark-frontmatter'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    { enforce: 'pre', ...mdx(
-      { 
+    {
+      enforce: "pre",
+      ...mdx({
         jsxImportSource: "react",
         remarkPlugins: [
-            remarkFrontmatter, // parses YAML frontmatter
-            [remarkMdxFrontmatter, { name: 'frontmatter' }], // exposes it as an export
-          ],
-     }) 
+          remarkFrontmatter, // parses YAML frontmatter,
+          remarkGfm, // support for github flavored markdown (tables, task lists, strikethrough, etc)
+          [remarkMdxFrontmatter, { name: "frontmatter" }], // exposes it as an export
+        ],
+      }),
     },
     react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
     tailwindcss(),
-    
   ],
   resolve: {
     alias: {
@@ -28,4 +30,4 @@ export default defineConfig({
     },
   },
   base: "/",
-})
+});
