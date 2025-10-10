387779a1f3b7e678925da078b4b231b977d8f4cd
tom-molotnikoff <tmolotnikoff@gmail.com>
2025-10-10 18:03:28 +0100

    Border styles for navigation bar!

--- tom-website/src/navigation/sidebar/HeaderWithSidebar.tsx
+++ tom-website/src/navigation/sidebar/HeaderWithSidebar.tsx
@@ -13,29 +13,31 @@ function HeaderWithSidebar() {
       <SidebarImpl />
       <main>
         <div
-          className={`sticky top-0 left-0 z-50 bg-background border-b border-border shadow-sm ${
+          className={`sticky top-0 left-0 z-50 rainbow-border-bottom ${
             isMobile ? "min-w-screen" : ""
           }`}
         >
-          <div className="px-5">
-            <div className="flex items-center justify-between space-x-5 h-16">
-              {/* Left side: Logo */}
-              <div className="grow items-center flex space-x-5">
-                <SidebarBurgerTrigger />
-                <Avatar>
-                  <AvatarImage
-                    src="/avatar/me.jpg"
-                    alt="Tom Molotnikoff Avatar"
-                  />
-                </Avatar>
-                <Link to="/" className="text-xl font-bold text-foreground">
-                  tom_molo.me
-                </Link>
-              </div>
-              {/* Right side: Navigation */}
-              <div className="flex flex-1 items-right justify-end space-x-4">
-                <div className="flex flex-1 max-w-30 items-center justify-end space-x-2">
-                  <ModeToggle />
+          <div className="rainbow-inner-bottom">
+            <div className="px-5">
+              <div className="flex items-center justify-between space-x-5 h-16">
+                {/* Left side: Logo */}
+                <div className="grow items-center flex space-x-5">
+                  <SidebarBurgerTrigger />
+                  <Avatar>
+                    <AvatarImage
+                      src="/avatar/me.jpg"
+                      alt="Tom Molotnikoff Avatar"
+                    />
+                  </Avatar>
+                  <Link to="/" className="text-xl font-bold text-foreground">
+                    tom_molo.me
+                  </Link>
+                </div>
+                {/* Right side: Navigation */}
+                <div className="flex flex-1 items-right justify-end space-x-4">
+                  <div className="flex flex-1 max-w-30 items-center justify-end space-x-2">
+                    <ModeToggle />
+                  </div>
                 </div>
               </div>
             </div>
