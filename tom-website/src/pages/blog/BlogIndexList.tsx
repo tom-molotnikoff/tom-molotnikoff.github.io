b7f8bf214b14e19b83c3614c896b499238048f2a
Tom Molotnikoff <tom.molotnikoff@cirata.com>
2025-10-12 11:29:46 +0100

    Dummy card for no matching filters on blog

--- tom-website/src/pages/blog/BlogIndexList.tsx
+++ tom-website/src/pages/blog/BlogIndexList.tsx
@@ -1,3 +1,5 @@
+import { TypographyH3, TypographyP } from "@/components/typography";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { useBlogFilter } from "./BlogFilterProvider";
 import BlogIndexCard from "./BlogIndexCard";
 
@@ -6,6 +8,25 @@ function BlogIndexList() {
 
   return (
     <>
+      {filteredPosts.length === 0 && (
+        <Card
+          className="w-full hover:bg-muted/90 glass"
+          data-testid="blog-post-card"
+        >
+          <CardHeader>
+            <CardTitle>
+              <TypographyH3>No posts found</TypographyH3>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <TypographyP>
+              Try adjusting your search or filter to find what you're looking
+              for.
+            </TypographyP>
+          </CardContent>
+        </Card>
+      )}
+
       {filteredPosts.map((post) => (
         <BlogIndexCard key={post.name} post={post} />
       ))}
